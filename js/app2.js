20ccc920cef4ba721972cfbdfc045a4d56cb30ff
NazrinMammadova <102550859+NazrinMammadova@users.noreply.github.com>
2022-07-03 18:27:17 +0400

    added fourth task

---
+++ js/app2.js
@@ -0,0 +1,39 @@
+
+ //task4
+
+
+
+if (input.substring(1,1) == 1)  
+{
+     alert("it is morning group")
+     document.body.style.backgroundColor = "yellow"
+}
+else if(input.substring(1,1) == 2)
+{
+    alert("it is afternoon group")
+    document.body.style.backgroundColor = "red"
+}
+else(input.substring(1,1) == 3)
+{
+alert("it is night group")
+document.body.style.backgroundColor = "blue"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
